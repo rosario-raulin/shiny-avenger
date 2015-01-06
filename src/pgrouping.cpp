282488fc6083a01a22b9d49c5618f146bb4a1d03
Rosario Raulin <post@rosario-raulin.de>
2015-01-06 18:36:01 +0100

    extend test case

--- src/pgrouping.cpp
+++ src/pgrouping.cpp
@@ -7,6 +7,10 @@ int main() {
 	GroupingAlgorithm* g = new RadixGrouping();
 	
 	Column fnord;
+	for (size_t i = 0; i < 999; ++i) {
+		fnord.push_back(i);
+	}
+	
 	std::vector<ColumnPtr> v;
 	v.push_back(&fnord);
 	g->groupBy(v);
