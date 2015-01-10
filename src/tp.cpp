dd505f1015ad6ac0179d05823d9cdcc9427af25d
Rosario Raulin <post@rosario-raulin.de>
2015-01-10 13:01:27 +0100

    add printUsage() function, handle integer conversion exception

--- src/tp.cpp
+++ src/tp.cpp
@@ -9,7 +9,8 @@
 #include <functional>
 #include <string>
 
-std::shared_ptr<Column> make_column(std::size_t size, std::size_t cardinality) {
+static std::shared_ptr<Column>
+make_column(std::size_t size, std::size_t cardinality) {
 	auto columnPtr = std::make_shared<Column>();
 	Column& column = *columnPtr;
 	column.resize(size);
@@ -24,14 +25,15 @@ std::shared_ptr<Column> make_column(std::size_t size, std::size_t cardinality) {
 	return columnPtr;
 }
 
-void print_column(std::shared_ptr<Column> columnPtr) {
+static void
+print_column(std::shared_ptr<Column> columnPtr) {
 	Column& column = *columnPtr;
 	for (auto i = 0; i < column.size(); ++i) {
 		std::cout << i << "\t" << column[i] << std::endl;
 	}
 }
 
-std::shared_ptr<IGroupingAlgorithm>
+static std::shared_ptr<IGroupingAlgorithm>
 make_grouping_algorithm(const std::string& algorithm) {
 	if (algorithm == "radix") {
 		return std::make_shared<RadixGrouping>();
@@ -42,6 +44,11 @@ make_grouping_algorithm(const std::string& algorithm) {
 	}
 }
 
+static inline void
+printUsage() {
+	std::cerr << "usage: tp algorithm-name test-size cardinality" << std::endl;
+}
+
 int main(int argc, char** argv) {
 	if (argc > 3) {
 		auto algo = make_grouping_algorithm(argv[1]);
@@ -50,36 +57,42 @@ int main(int argc, char** argv) {
 			return 1;
 		}
 		
-		auto testsize = std::stoi(argv[2]);
-		auto cardinality = std::stoi(argv[3]);
-		
-		auto column = make_column(testsize, cardinality);
-
-		std::vector<ColumnPtr> columns;
-		columns.emplace_back(column.get());
-
-		auto start = std::chrono::system_clock::now();
-		auto res = algo->groupBy(columns);
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
-		
-		std::cout << "Grouping took " << (duration.count() / 1000.0) << " seconds." << std::endl;
-		
-		auto groups = 1; // there is at least one group
-		auto ptr = res.get();
-		for (std::size_t i = 0; i < column->size()-1; ++i) {
-			auto v1 = (*column)[ptr[i]];
-			auto v2 = (*column)[ptr[i+1]];
+		try {
+			auto testsize = std::stoi(argv[2]);
+			auto cardinality = std::stoi(argv[3]);
 			
-			if (v1 != v2) {
-				//std::cout << v1 << " and " << v2 << " differ." << std::endl;
-				++groups;
+			auto column = make_column(testsize, cardinality);
+			
+			std::vector<ColumnPtr> columns;
+			columns.emplace_back(column.get());
+			
+			auto start = std::chrono::system_clock::now();
+			auto res = algo->groupBy(columns);
+			auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
+			
+			std::cout << "Grouping took " << (duration.count() / 1000.0) << " seconds." << std::endl;
+			
+			auto groups = 1; // there is at least one group
+			auto ptr = res.get();
+			for (std::size_t i = 0; i < column->size()-1; ++i) {
+				auto v1 = (*column)[ptr[i]];
+				auto v2 = (*column)[ptr[i+1]];
+				
+				if (v1 != v2) {
+					//std::cout << v1 << " and " << v2 << " differ." << std::endl;
+					++groups;
+				}
 			}
+			
+			std::cout << "Found " << groups << " groups." << std::endl;
+			return 0;
+		} catch (const std::invalid_argument& e) {
+			std::cerr << "error: invalid arguments, see usage" << std::endl;
+			printUsage();
+			return 1;
 		}
-		
-		std::cout << "Found " << groups << " groups." << std::endl;
-		
-		return 0;
 	} else {
+		printUsage();
 		return 1;
 	}
 }
