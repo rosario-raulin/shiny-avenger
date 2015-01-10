366bf4c9cd60cfeff4ebcd64c251d4be51db10f2
Rosario Raulin <post@rosario-raulin.de>
2015-01-10 13:04:59 +0100

    factor out test function (for later use in CoGaDB)

--- src/tp.cpp
+++ src/tp.cpp
@@ -49,6 +49,34 @@ printUsage() {
 	std::cerr << "usage: tp algorithm-name test-size cardinality" << std::endl;
 }
 
+static void
+testGroupingAlgorithm(std::shared_ptr<IGroupingAlgorithm> algo, std::size_t testsize, std::size_t cardinality) {
+	auto column = make_column(testsize, cardinality);
+	
+	std::vector<ColumnPtr> columns;
+	columns.emplace_back(column.get());
+	
+	auto start = std::chrono::system_clock::now();
+	auto res = algo->groupBy(columns);
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
+	
+	std::cout << "Grouping took " << (duration.count() / 1000.0) << " seconds." << std::endl;
+	
+	auto groups = 1; // there is at least one group
+	auto ptr = res.get();
+	for (std::size_t i = 0; i < column->size()-1; ++i) {
+		auto v1 = (*column)[ptr[i]];
+		auto v2 = (*column)[ptr[i+1]];
+		
+		if (v1 != v2) {
+			//std::cout << v1 << " and " << v2 << " differ." << std::endl;
+			++groups;
+		}
+	}
+	
+	std::cout << "Found " << groups << " groups." << std::endl;
+}
+
 int main(int argc, char** argv) {
 	if (argc > 3) {
 		auto algo = make_grouping_algorithm(argv[1]);
@@ -60,31 +88,7 @@ int main(int argc, char** argv) {
 		try {
 			auto testsize = std::stoi(argv[2]);
 			auto cardinality = std::stoi(argv[3]);
-			
-			auto column = make_column(testsize, cardinality);
-			
-			std::vector<ColumnPtr> columns;
-			columns.emplace_back(column.get());
-			
-			auto start = std::chrono::system_clock::now();
-			auto res = algo->groupBy(columns);
-			auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
-			
-			std::cout << "Grouping took " << (duration.count() / 1000.0) << " seconds." << std::endl;
-			
-			auto groups = 1; // there is at least one group
-			auto ptr = res.get();
-			for (std::size_t i = 0; i < column->size()-1; ++i) {
-				auto v1 = (*column)[ptr[i]];
-				auto v2 = (*column)[ptr[i+1]];
-				
-				if (v1 != v2) {
-					//std::cout << v1 << " and " << v2 << " differ." << std::endl;
-					++groups;
-				}
-			}
-			
-			std::cout << "Found " << groups << " groups." << std::endl;
+			testGroupingAlgorithm(algo, testsize, cardinality);
 			return 0;
 		} catch (const std::invalid_argument& e) {
 			std::cerr << "error: invalid arguments, see usage" << std::endl;
