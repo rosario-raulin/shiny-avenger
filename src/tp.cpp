393bd0290bf5936566bdf97265ebeeb992d9f392
Rosario Raulin <post@rosario-raulin.de>
2015-01-10 01:37:41 +0100

    add option to choose algorithm via command line parameter

--- src/tp.cpp
+++ src/tp.cpp
@@ -7,6 +7,7 @@
 #include <random>
 #include <chrono>
 #include <functional>
+#include <string>
 
 std::shared_ptr<Column> make_column(std::size_t size, std::size_t cardinality) {
 	auto columnPtr = std::make_shared<Column>();
@@ -38,24 +39,40 @@ typename TimeType::rep measureTime(Functor function, Args&&... args) {
 	return duration.count();
 }
 
-int main() {
-	auto algo = std::make_shared<RadixGrouping>();
-	auto column = make_column(TESTSIZE, CARDINALITY);
-	// print_column(column);
-	std::vector<ColumnPtr> columns;
-	columns.emplace_back(column.get());
-	
-	auto fn = [algo](const std::vector<ColumnPtr>& columns) { algo->groupBy(columns); };
-	auto duration = measureTime(fn, columns);
-	std::cout << "Grouping took " << duration / 1000.0 << " seconds." << std::endl;
-	//auto ptr = algo.groupBy(columns);
-	
-	for (auto i = 0; i < CARDINALITY; ++i) {
-		std::cout << i << " maps to " << VALUES[i] << " values." << std::endl;
+std::shared_ptr<IGroupingAlgorithm>
+make_grouping_algorithm(const std::string& algorithm) {
+	if (algorithm == "radix") {
+		return std::make_shared<RadixGrouping>();
+	} else if (algorithm == "simple") {
+		return std::make_shared<HashBasedGrouping>();
+	} else {
+		return nullptr;
 	}
-	
-	// auto printer = [](const std::size_t& x) { std::cout << x << std::endl; };
-	// std::for_each(ptr.get(), ptr.get() + column->size(), printer);
+}
+
+int main(int argc, char** argv) {
+	if (argc > 1) {
+		auto algo = make_grouping_algorithm(argv[1]);
+		if (algo == nullptr) {
+			std::cerr << "error: unknown grouping algorithm!" << std::endl;
+			return 1;
+		}
 		
-	return 0;
+		auto column = make_column(TESTSIZE, CARDINALITY);
+
+		std::vector<ColumnPtr> columns;
+		columns.emplace_back(column.get());
+		
+		auto fn = [algo](const std::vector<ColumnPtr>& columns) { algo->groupBy(columns); };
+		auto duration = measureTime(fn, columns);
+		std::cout << "Grouping took " << duration / 1000.0 << " seconds." << std::endl;
+		
+		for (auto i = 0; i < CARDINALITY; ++i) {
+			std::cout << i << " maps to " << VALUES[i] << " values." << std::endl;
+		}
+		
+		return 0;
+	} else {
+		return 1;
+	}
 }
