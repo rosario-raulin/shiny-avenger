c27790bb5b90feefa7de5cd4600220ab362c8740
Rosario Raulin <post@rosario-raulin.de>
2015-01-10 01:17:30 +0100

    move testing constants to CommonTypes

--- src/CommonTypes.hpp
+++ src/CommonTypes.hpp
@@ -2,10 +2,14 @@
 #define HAVE_COMMON_TYPES_HPP
 
 #include <vector>
+#include <memory>
 
 using PositionList = std::size_t;
 using PositionListPtr = std::shared_ptr<PositionList>;
 using Column = std::vector<std::size_t>;
 using ColumnPtr = Column*;
+
+#define TESTSIZE 10000
+#define CARDINALITY 10000
 
 #endif
