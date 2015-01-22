f51fd8a6ce6bc6da133329d1e7c64ef96b518daf
Rosario Raulin <post@rosario-raulin.de>
2015-01-22 14:00:43 +0100

    make number of threads, tasks and bits for hashing parameters

--- src/RadixGrouping.cpp
+++ src/RadixGrouping.cpp
@@ -7,26 +7,20 @@
 #include <algorithm>
 #include <unordered_map>
 
-#define NUMBER_OF_RELEVANT_BITS 11
-
 using Histogram = Matrix<std::size_t>;
-
-static inline std::size_t
-hash(std::size_t value) {
-	return value & ((1 << NUMBER_OF_RELEVANT_BITS) - 1);
-}
 
 static void
 indexJob(std::size_t id,
 	Column& column,
 	Histogram& histogram,
+	RadixGrouping& rgrouping,
 	std::size_t lower,
 	std::size_t upper,
 	std::shared_ptr<std::size_t> reordered,
 	std::shared_ptr<std::size_t> indices,
 	std::shared_ptr<std::size_t> destOutput)
 {
-	auto table_size = (1 << NUMBER_OF_RELEVANT_BITS);
+	auto table_size = (1 << rgrouping.number_of_bits());
 
 	std::unique_ptr<std::size_t[]> dest(new std::size_t[table_size]());
 
@@ -36,7 +30,7 @@ indexJob(std::size_t id,
 		}
 	}
 
-	for (auto i = id; i < NUMBER_OF_TASKS; ++i) {
+	for (auto i = id; i < rgrouping.number_of_tasks(); ++i) {
 		for (auto j = 1; j < table_size; ++j) {
 			dest[j] += histogram.get(i, j-1);
 		}
@@ -47,7 +41,7 @@ indexJob(std::size_t id,
 	}
 
 	for (auto i = lower; i < upper; ++i) {
-		auto index = hash(column[i]);
+		auto index = rgrouping.hash(column[i]);
 		(indices.get())[dest[index]] = i;
 		(reordered.get())[dest[index]] = column[i];
 		++dest[index];
@@ -58,15 +52,16 @@ static void
 histJob(std::size_t id,
 	Column& column,
 	Histogram& histogram,
+	RadixGrouping& rgrouping,
 	std::size_t lower,
 	std::size_t upper)
 {
 	for (auto i = lower; i < upper; ++i) {
-		auto index = hash(column[i]);
+		auto index = rgrouping.hash(column[i]);
 		++histogram.get(id, index);
 	}
 
-	auto table_size = (1 << NUMBER_OF_RELEVANT_BITS);
+	auto table_size = (1 << rgrouping.number_of_bits());
 	auto prefix_sum = 0;
 	for (auto i = 0; i < table_size; ++i) {
 		prefix_sum += histogram.get(id, i);
@@ -79,11 +74,12 @@ void startAndEndJobs(Function fn,
 	ThreadPool& pool,
 	Column& column,
 	Histogram& histogram,
+	RadixGrouping& rgrouping,
 	Args&&... args)
 {
-	std::size_t chunk_size = column.size() / NUMBER_OF_TASKS;
-	std::size_t diff_cases = column.size() % NUMBER_OF_TASKS;
-	std::size_t regular_cases = NUMBER_OF_TASKS - diff_cases;
+	std::size_t chunk_size = column.size() / rgrouping.number_of_tasks();
+	std::size_t diff_cases = column.size() % rgrouping.number_of_tasks();
+	std::size_t regular_cases = rgrouping.number_of_tasks() - diff_cases;
 
 	using ResultType = std::shared_future<void>;
 	std::vector<ResultType> results;
@@ -92,7 +88,7 @@ void startAndEndJobs(Function fn,
 		std::size_t lower = chunk_size * i;
 		std::size_t upper = lower + chunk_size;
 
-		auto job = [&, i, lower, upper]() { fn(i, column, histogram, lower, upper, args...); };
+		auto job = [&, i, lower, upper]() { fn(i, column, histogram, rgrouping, lower, upper, args...); };
 		results.emplace_back(pool.addJob(job));
 	}
 
@@ -101,7 +97,7 @@ void startAndEndJobs(Function fn,
 	for (auto i = 0; i < diff_cases; ++i) {
 		upper = lower + chunk_size + 1;
 		auto job = [&, i, regular_cases, lower, upper]() {
-			fn(i + regular_cases, column, histogram, lower, upper, std::forward<Args>(args)...);
+			fn(i + regular_cases, column, histogram, rgrouping, lower, upper, std::forward<Args>(args)...);
 		};
 		results.emplace_back(pool.addJob(job));
 
@@ -132,16 +128,25 @@ void localGrouping(std::shared_ptr<std::size_t> reordered,
 	}
 }
 
+RadixGrouping::RadixGrouping(size_t number_of_threads,
+	size_t number_of_tasks,
+	size_t number_of_bits) :
+ 	_number_of_threads(number_of_threads),
+	_number_of_tasks(number_of_tasks),
+	_number_of_bits(number_of_bits)
+{
+}
+
 PositionListPtr
 RadixGrouping::groupBy(const std::vector<ColumnPtr>& columns) {
-	auto table_size = (1 << NUMBER_OF_RELEVANT_BITS);
+	auto table_size = (1 << _number_of_bits);
 
 	Column& column = *(columns[0]);
-	Histogram histogram(NUMBER_OF_TASKS, table_size);
-	ThreadPool pool(NUMBER_OF_THREADS);
+	Histogram histogram(_number_of_tasks, table_size);
+	ThreadPool pool(_number_of_threads);
 
 	// Step 1: build local histograms
-	startAndEndJobs(histJob, pool, column, histogram);
+	startAndEndJobs(histJob, pool, column, histogram, *this);
 
 	// little workaround: C++11 doesn't have shared_ptr arrays, so we need a custom deallocator
 	std::default_delete<std::size_t[]> array_deleter;
@@ -150,7 +155,7 @@ RadixGrouping::groupBy(const std::vector<ColumnPtr>& columns) {
 
 	// Step 2: calculate start/end indicies
 	std::shared_ptr<std::size_t> destOutput(new std::size_t[table_size], array_deleter);
-	startAndEndJobs(indexJob, pool, column, histogram, reordered, indices, destOutput);
+	startAndEndJobs(indexJob, pool, column, histogram, *this, reordered, indices, destOutput);
 
 	// Step 3: for each partition, create local grouping jobs
 	using ResultType = std::shared_future<void>;
--- src/RadixGrouping.hpp
+++ src/RadixGrouping.hpp
@@ -3,11 +3,29 @@
 
 #include "GroupingAlgorithm.hpp"
 
-#define NUMBER_OF_TASKS 16
-
 class RadixGrouping : public IGroupingAlgorithm {
 public:
+	RadixGrouping(size_t _number_of_threads,
+		size_t number_of_tasks,
+		size_t number_of_bits);
 	PositionListPtr groupBy(const std::vector<ColumnPtr>& columns);
+
+	size_t number_of_bits() const {
+		return _number_of_bits;
+	}
+
+	size_t number_of_tasks() const {
+		return _number_of_tasks;
+	}
+
+	size_t hash(size_t value) const {
+		return value & ((1 << _number_of_bits) - 1);
+	}
+
+private:
+	const size_t _number_of_threads;
+	const size_t _number_of_tasks;
+	const size_t _number_of_bits;
 };
 
 #endif
